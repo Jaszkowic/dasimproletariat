8f09985801726c5fc40e5c40ff26305740c290d3
Jonas Jaszkowic <jonasjaszkowic@googlemail.com>
2018-10-10 19:13:45 +0200

    optimized css

--- collapseFunctions.js
+++ collapseFunctions.js
@@ -1,17 +1,19 @@
 let collapseVisible = {};
 function toggleCollapseElement(targetName, parentName) {
-    
+
     if(collapseVisible[targetName] === undefined) {
         collapseVisible[targetName] = false;
     }
     setTimeout(function() {
+        const mainNavHeight = $("#mainNav").innerHeight();
         if(!collapseVisible[targetName]) {
+            console.log(mainNavHeight);
             $([document.documentElement, document.body]).animate({
-                scrollTop: $(`#${targetName}`).offset().top - $("#mainNav").height() - 16
+                scrollTop: $(`#${targetName}`).offset().top - mainNavHeight - 16
             }, 100);
         } else {
             $([document.documentElement, document.body]).animate({
-                scrollTop: $(`#${parentName}`).offset().top - $("#mainNav").height() - 16
+                scrollTop: $(`#${parentName}`).offset().top - mainNavHeight - 16
             }, 100);
         }
         collapseVisible[targetName] = collapseVisible[targetName] = !collapseVisible[targetName];
