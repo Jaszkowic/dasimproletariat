90a05fa7a86a0871020eac34379c88e2fbe0a0d3
Jonas Jaszkowic <jonasjaszkowic@googlemail.com>
2019-05-19 19:11:24 +0200

    add calendar export, cleanup images

--- insertImages.js
+++ insertImages.js
@@ -1,5 +1,10 @@
-window.onload = function() {
-    let shows = [{name: '26062018', numImgs: 2}, {name: '08102018', numImgs: 27}, {name: '13112018', numImgs: 6}, {name: '26032019', numImgs: 5}, {name: '14052019', numImgs: 10}];
+window.onload = function () {
+    let shows = [
+        {name: '26062018', numImgs: 2},
+        {name: '08102018', numImgs: 27},
+        {name: '13112018', numImgs: 6},
+        {name: '26032019', numImgs: 5},
+        {name: '14052019', numImgs: 8}];
     for (let n = 0; n < shows.length; n++) {
         let show = shows[n];
         for (let i = 1; i <= show.numImgs; i++) {
@@ -8,7 +13,7 @@ window.onload = function() {
             let htmlToInsert = `<div class=\"carousel-item\"><img src=\"img/loading.png\" data-src=\"img/shows/${show.name}/${scrName}" alt=\"\" style=\"width: 100%; height: 100%; object-fit: contain;\"></div>`;
             let chosenHtml = (i === 1) ? initHtmlToInsert : htmlToInsert;
             let showId = `show${show.name}imagesContainer`;
-            document.getElementById(showId).insertAdjacentHTML('afterbegin',chosenHtml);
+            document.getElementById(showId).insertAdjacentHTML('afterbegin', chosenHtml);
         }
     }
 }
